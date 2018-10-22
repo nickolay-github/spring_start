aabcff548d86d875ecd59f2618eb6d2eb18d14de
nickolay-github <columbine.bullet@gmail.com>
2018-10-23 02:44:00 +0300

    add feature for upload files on server

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -4,19 +4,27 @@ import com.example.demo.domain.Message;
 import com.example.demo.domain.User;
 import com.example.demo.repos.MessageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
+import java.util.UUID;
 
 @Controller
 public class MainController {
     @Autowired
     private MessageRepository messageRepository;
+
+    @Value("${upload.path}")
+    private String uploadPath;
 
     @GetMapping("/")
     public String greeting() {
@@ -26,6 +34,8 @@ public class MainController {
     @GetMapping("/main")
     public String main(@RequestParam(required = false) String filter, Model model) {
         Iterable<Message> messages = messageRepository.findAll();
+
+//        messages.forEach(message -> messageRepository.delete(message));   /** deleting all messages
 
         if (filter != null && !filter.isEmpty()) {
             messages = messageRepository.findByTag(filter);
@@ -40,9 +50,25 @@ public class MainController {
     @PostMapping("/main")
     public String add(@AuthenticationPrincipal User user,
                       @RequestParam String text,
-                      @RequestParam String tag,
-                      Map<String, Object> model) {
+                      @RequestParam String tag, Map<String, Object> model,
+                      @RequestParam("file") MultipartFile file) throws IOException {
+
+
         Message message = new Message(text, tag, user);
+
+        if (file != null && !file.getOriginalFilename().isEmpty()) {
+
+            File uploadDir = new File(uploadPath);
+            if (!uploadDir.exists()){
+                uploadDir.mkdir();
+            }
+
+            String uuidName  = UUID.randomUUID().toString();
+            String resultFilename = uuidName + "." + file.getOriginalFilename();
+
+            file.transferTo(new File(uploadPath + "/" + resultFilename));
+            message.setFilename(resultFilename);
+        }
 
         messageRepository.save(message);
 
--- src/main/java/com/example/demo/domain/Role.java
+++ src/main/java/com/example/demo/domain/Role.java
@@ -6,7 +6,6 @@ import org.springframework.security.core.GrantedAuthority;
 public enum Role implements GrantedAuthority {
     ADMIN, USER;
 
-
     @Override
     public String getAuthority() {
         return name();
